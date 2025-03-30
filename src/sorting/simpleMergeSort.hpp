07fb47c0b9c4dfbf27c0b046c4b23db6d86c0056
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-03-30 20:13:08 +0530

    Changed folder structure

---
+++ src/sorting/simpleMergeSort.hpp
@@ -0,0 +1,47 @@
+/**
+ * @file simpleMergeSort.hpp
+ * @brief Provides a class for merge sort implementation on integer vectors
+ */
+
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include <vector>
+
+/**
+ * @class MergeSort
+ * @brief Implements the merge sort algorithm for integer vectors
+ * 
+ * This class provides functionality to sort a vector of integers
+ * using the merge sort algorithm, which has O(n log n) time complexity.
+ */
+class MergeSort {
+private:
+    std::vector<int> *nums; /**< Pointer to the vector to be sorted */
+
+public:
+    /**
+     * @brief Constructor for MergeSort
+     * @param nums Pointer to the vector to be sorted
+     */
+    MergeSort(std::vector<int> *nums);
+    
+    /**
+     * @brief Destructor for MergeSort
+     */
+    ~MergeSort();
+    
+    /**
+     * @brief Initiates the sorting process
+     */
+    void sort();
+    
+    /**
+     * @brief Performs recursive merge sort on a subarray
+     * @param startIndex The starting index of the subarray
+     * @param endIndex The ending index of the subarray
+     */
+    void recursiveSort(int startIndex, int endIndex);
+};
+
+#endif
